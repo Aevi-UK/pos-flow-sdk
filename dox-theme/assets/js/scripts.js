aaf6fd335428b5a3580dba5912d2d2b47d432b31
Andreas Borglin <andreas@jakoon.co.uk>
2019-02-18 15:22:09 +0000

    Major work on the documentation

--- dox-theme/assets/js/scripts.js
+++ dox-theme/assets/js/scripts.js
@@ -21,5 +21,15 @@
 	 */
 	$( document ).ready( function() {
 		$( '.js-post' ).find( 'table' ).wrap( '<div class="o-responsive-table"></div>' );
+
+		// augment all external links on the site to open in new tab
+		var links = document.links;
+		for (var i = 0, linksLength = links.length; i < linksLength; i++) {
+			console.log("hi: " + links[i]);
+			if (links[i].hostname != window.location.hostname) {
+				links[i].target = '_blank';
+				links[i].className += ' externalLink';
+			}
+		}
 	} );
 }( jQuery ) );
